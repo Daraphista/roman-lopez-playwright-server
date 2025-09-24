5dd6d9b837a312802307cdd22ada7662f36e7e30
Daraphista <81943522+Daraphista@users.noreply.github.com>
2025-09-24 15:27:13 +0800

    Update example script

--- scripts/example.js
+++ scripts/example.js
@@ -8,13 +8,11 @@ export default async function run(input = {}) {
   const page = await context.newPage();
 
   const url = input.url || 'https://example.com';
-  console.log(`🌐 Starting Job: ${url}`);
   await page.goto(url);
 
   await humanPause(page, 2000, 5000);
   
   const title = await page.title();
-  console.log(`📄 Finished Job: ${title}`);
 
   await context.close();
   await browser.close();
